4950223f99d914c46ff43ad78864d01794db8c9f
Samuel Longchamps <slongchamps@audiokinetic.com>
2018-06-29 14:31:05 -0400

    Added default to build, fixed alias to 'b' instead of 'p' and added support for '?' argument to list available targets.

--- gak/commands/build.py
+++ gak/commands/build.py
@@ -6,15 +6,37 @@ from gak.utils import retrieve_config
 
 class BuildGakCommand(GakCommand):
     def init_parser(self, subparsers):
-        build_parser = subparsers.add_parser("build", aliases=['p'],
+        build_parser = subparsers.add_parser("build", aliases=['b'],
                                              help="Build stuff")
-        build_parser.add_argument("what", help="Stuff to build")
+        build_parser.add_argument("what", default="default", nargs="?", help="Stuff to build")
         build_parser.add_argument("-c", "--configure", action="store_true", help="Configure before building")
         build_parser.set_defaults(func=self.command)
 
     def command(self, args, root):
         config = retrieve_config("build", root)
-        what = config.get(args.what)
+        if not config:
+            sys.stderr.write("No user configuration defined for 'build'\n")
+            return 1
+
+        default = config.get("default")
+        if args.what == "?" and config:
+            default_msg = ""
+            if default:
+                default_msg = " (default: {})".format(default)
+                config.pop("default")
+
+            print("Defined targets{}:".format(default_msg))
+            list(map(print, ("* {}".format(k) for k in config.keys())))
+            return 0
+
+        if args.what == "default" and default:
+            what = config.get(default)
+            if not what:
+                print("No configuration defined for default '{}'".format(default))
+                return 1
+        else:
+            what = config.get(args.what)
+
         if what is not None:
             if args.configure or args.what == "configure":
                 config_commands = config.get("configure")
@@ -34,6 +56,7 @@ class BuildGakCommand(GakCommand):
                 print("Nothing defined to build!")
         else:
             print("No configuration defined for '{}'".format(args.what))
+
         return 1
 
 
