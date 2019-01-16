f091863e711599585f8a28f28f13bf1b6e92855a
Samuel Longchamps <slongchamps@audiokinetic.com>
2019-01-16 11:37:50 -0500

    Added prefix override for mkdev, upgraded dev status to beta and version to 0.2.0

--- gak/commands/mkdev.py
+++ gak/commands/mkdev.py
@@ -10,26 +10,31 @@ class MkdevGakCommand(GakCommand):
                                              help="Rename the current branch with a dev prefix")
         mkdev_parser.add_argument("-t", "--toggle", action="store_true",
                                   help="Toggle between the dev prefix")
+        mkdev_parser.add_argument("-p", "--prefix", type=str, help="Prefix override to use")
         mkdev_parser.set_defaults(func=self.command)
 
     def command(self, args, root):
-        user_config = retrieve_user_config()
-        prefix = user_config.get("mkdev", {}).get("prefix")
-        if not prefix:
-            print("[WARNING] No dev prefix defined, default to 'dev'")
-            prefix = "dev"
+        prefix = "dev"
+        if args.prefix:
+            prefix = args.prefix
+        else:
+            user_config = retrieve_user_config()
+            user_prefix = user_config.get("mkdev", {}).get("prefix")
+            if not user_prefix:
+                print("[WARNING] No dev prefix defined, default to 'dev'")
+            else:
+                prefix = user_prefix
 
         current_branch = subprocess.getoutput("git rev-parse --abbrev-ref HEAD")
         split_current_branch = current_branch.split("/")
-        is_dev_branch = split_current_branch[0] == prefix
+        is_dev_branch = "/".join(split_current_branch[0:-1]) == prefix
 
         cmd = None
         if not is_dev_branch:
-            cmd = "git branch -m {current} {prefix}/{current}".format(
-                current=current_branch, prefix=prefix)
+            cmd = f"git branch -m {current_branch} {prefix}/{current_branch}"
         elif args.toggle:
-            cmd = "git branch -m {prefix}/{suffix} {suffix}".format(
-                prefix=prefix, suffix=split_current_branch[1])
+            suffix = split_current_branch[-1]
+            cmd = f"git branch -m {prefix}/{suffix} {suffix}"
 
         if cmd:
             print(cmd)
--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages
 setup(
     name = 'gak',
     packages = find_packages(),
-	scripts=['scripts/gak'],
-    version = '0.1.1',
+    scripts=['scripts/gak'],
+    version = '0.2.0',
     description = 'Command-line tool to make local Git repo interaction more efficient',
     author = 'Samuel Longchamps',
     author_email = 'samuel.longchamps@usherbrooke.ca',
@@ -18,6 +18,6 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: MIT',
         'Topic :: Utilities',
-        'Development Status :: 3 - Alpha'
+        'Development Status :: 4 - Beta'
     ],
 )
